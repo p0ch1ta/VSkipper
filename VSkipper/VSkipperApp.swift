552866bbc0280c2e8b738c5283143885736a2ef9
nv-mac <nikolay.dev@smartpayments.ee>
2023-03-18 21:32:54 +0400

    Initial Commit

---
+++ VSkipper/VSkipperApp.swift
@@ -0,0 +1,12 @@
+//  Created by p0ch1ta on 18/03/2023 for project VSkipper
+
+import SwiftUI
+
+@main
+struct VSkipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
