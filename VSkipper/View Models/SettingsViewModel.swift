e59e73588a2b8779d93fac5e9d724326759f34d0
nv-mac <heavy.flag5729@yokata.me>
2023-09-25 15:26:03 +0200

    Remove legacy settings

--- VSkipper/View Models/SettingsViewModel.swift
+++ VSkipper/View Models/SettingsViewModel.swift
@@ -7,14 +7,6 @@ class SettingsViewModel: ObservableObject {
 
     let defaults = UserDefaults.standard
 
-    @Published var introDuration: Int = 0
-
-    @Published var outroDuration: Int = 0
-
-    @Published var introScanEnd: Int = 0
-
-    @Published var outroScanStart: Int = 0
-
     @Published var vlcPort: Int = 8080
 
     @Published var vlcPassword: String = ""
@@ -22,45 +14,9 @@ class SettingsViewModel: ObservableObject {
     @Published var vlcExecutablePath: String = ""
 
     init() {
-        introDuration = defaults.integer(forKey: SettingsKey.introDuration.rawValue)
-        outroDuration = defaults.integer(forKey: SettingsKey.outroDuration.rawValue)
-        introScanEnd = defaults.integer(forKey: SettingsKey.introScanEnd.rawValue)
-        outroScanStart = defaults.integer(forKey: SettingsKey.outroScanStart.rawValue)
         vlcPort = defaults.integer(forKey: SettingsKey.vlcPort.rawValue)
         vlcPassword = defaults.string(forKey: SettingsKey.vlcPassword.rawValue) ?? ""
         vlcExecutablePath = defaults.string(forKey: SettingsKey.vlcExecutablePath.rawValue) ?? ""
-    }
-
-    func getIntroStartTime() throws {
-        let vlcApi = VLCApi(port: vlcPort, password: vlcPassword)
-        introDuration = try vlcApi.getCurrentTime()
-    }
-
-    func getOutroStartTime() throws {
-        let vlcApi = VLCApi(port: vlcPort, password: vlcPassword)
-        outroDuration = try vlcApi.getCurrentTime()
-    }
-
-    func calculateIntroDuration() throws {
-        let vlcApi = VLCApi(port: vlcPort, password: vlcPassword)
-        let introEndTime = try vlcApi.getCurrentTime()
-        introDuration = introEndTime - introDuration
-    }
-
-    func calculateOutroDuration() throws {
-        let vlcApi = VLCApi(port: vlcPort, password: vlcPassword)
-        let outroEndTime = try vlcApi.getCurrentTime()
-        outroDuration = outroEndTime - outroDuration
-    }
-
-    func getIntroScanEndTime() throws {
-        let vlcApi = VLCApi(port: vlcPort, password: vlcPassword)
-        introScanEnd = try vlcApi.getCurrentTime()
-    }
-
-    func getOutroScanStartTime() throws {
-        let vlcApi = VLCApi(port: vlcPort, password: vlcPassword)
-        outroScanStart = try vlcApi.getCurrentTime()
     }
 
     func save() throws {
@@ -68,11 +24,6 @@ class SettingsViewModel: ObservableObject {
         if !vlcExecutablePath.hasSuffix(APP.FileName.vlcAppName) {
             throw SettingsError.incorrectVlcPath
         }
-
-        defaults.set(introDuration, forKey: SettingsKey.introDuration.rawValue)
-        defaults.set(outroDuration, forKey: SettingsKey.outroDuration.rawValue)
-        defaults.set(introScanEnd, forKey: SettingsKey.introScanEnd.rawValue)
-        defaults.set(outroScanStart, forKey: SettingsKey.outroScanStart.rawValue)
         defaults.set(vlcPort, forKey: SettingsKey.vlcPort.rawValue)
         defaults.set(vlcPassword, forKey: SettingsKey.vlcPassword.rawValue)
         defaults.set(vlcExecutablePath, forKey: SettingsKey.vlcExecutablePath.rawValue)
